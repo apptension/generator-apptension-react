b89567d8273123a272dbeaf6e7bf500ef2430238
Patryk Ziemkowski <pziemkowski@apptension.com>
2016-11-17 12:30:40 +0100

    Add react hot reload to main.js

--- generators/app/templates/app/main.js
+++ generators/app/templates/app/main.js
@@ -3,7 +3,7 @@ import './src/main.scss';
 import 'babel-polyfill';
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render, unmountComponentAtNode} from 'react-dom';
 import {Router, browserHistory} from 'react-router';
 import {syncHistoryWithStore} from 'react-router-redux';
 import {Provider} from 'react-redux';
@@ -14,49 +14,41 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import configureStore from './src/modules/configureStore';
 import {IntlProvider, addLocaleData} from './src/modules/utils';
-import routes from './src/routes';
 
-function startApp() {
-  // Needed for onTouchTap
-  // Check this repo:
-  // https://github.com/zilverline/react-tap-event-plugin
-  injectTapEventPlugin();
+// Needed for onTouchTap
+// Check this repo:
+// https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
 
-  // add to React-Intl locale data
-  addLocaleData();
+// add to React-Intl locale data
+addLocaleData();
 
-  const store = configureStore();
-
-  ReactGA.initialize(envConfig.googleAnalytics.clientID, {
-    ...envConfig.googleAnalytics.options,
-    gaOptions: {
-      userId: store.getState().getIn(['user', 'data', 'id'], null)
-    }
-  });
-
-  const syncedBrowserHistory = syncHistoryWithStore(browserHistory, store, {
-    selectLocationState(state) {
-      return state.get('routing').toJS();
-    }
-  });
-
-  const reactRoot = window.document.getElementById('react-root');
-
-  if (__DEBUG__) {
-    let DevToolsComponent = require('src/widgets/devtools/devTools.component');
-    const devToolsRoot = window.document.createElement('div');
-
-    window.document.body.appendChild(devToolsRoot);
-
-    ReactDOM.render(
-      <Provider store={store}>
-        <DevToolsComponent/>
-      </Provider>,
-      devToolsRoot
-    );
+const store = configureStore();
+const syncedBrowserHistory = syncHistoryWithStore(browserHistory, store, {
+  selectLocationState(state) {
+    return state.get('routing').toJS();
   }
+});
+const reactRoot = window.document.getElementById('react-root');
 
-  ReactDOM.render(
+if (__DEBUG__) {
+  let DevToolsComponent = require('src/widgets/devtools/devTools.component');
+  const devToolsRoot = window.document.createElement('div');
+
+  window.document.body.appendChild(devToolsRoot);
+
+  render(
+    <Provider store={store}>
+      <DevToolsComponent/>
+    </Provider>,
+    devToolsRoot
+  );
+}
+
+function renderApp() {
+  const routes = require('./src/routes').default;
+
+  render(
     <Provider store={store}>
       <IntlProvider locale="en">
         <Router history={syncedBrowserHistory} routes={routes}/>
@@ -64,6 +56,24 @@ function startApp() {
     </Provider>,
     reactRoot
   );
+}
+
+function startApp() {
+  ReactGA.initialize(envConfig.googleAnalytics.clientID, {
+    ...envConfig.googleAnalytics.options,
+    gaOptions: {
+      userId: store.getState().getIn(['user', 'data', 'id'], null)
+    }
+  });
+
+  renderApp();
+
+  if (module.hot) {
+    module.hot.accept('./src/routes', () => {
+      unmountComponentAtNode(reactRoot);
+      renderApp();
+    });
+  }
 }
 
 if (!global.Intl) {
